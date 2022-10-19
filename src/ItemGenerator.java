f43e179e5964689b557c6dc04bcd5b9b9711d2eb
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-10-19 14:57:17 +0300

    comments added

--- src/ItemGenerator.java
+++ src/ItemGenerator.java
@@ -1,8 +1,10 @@
 public abstract class ItemGenerator {
+
+    // абстрактный метод, при расширении которого в наследниках возвращается объект соответствующего класса-награды
     public abstract GameItem createPrize ();
 
     public void openReward()
-    {
+    { // инициализация экземпляра класса "...Reward", полученного в createPrize() и вывод сообщения
         GameItem rewardPrize = createPrize();
         rewardPrize.open();
     }
