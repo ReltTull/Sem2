528e6dce542bb3ac8840314ad4585216c07f5cee
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-10-19 14:56:35 +0300

    comments added

--- src/ArmorReward.java
+++ src/ArmorReward.java
@@ -5,7 +5,7 @@ public class ArmorReward implements GameItem{
     Random random = new Random();
     @Override
     public void open()
-    {
+    { // реализация интерфейса GameItem под класс. В данном случае добавлена система псевдослучайных уровней Random
         int lvl = random.nextInt(1, 10);
         System.out.println("You got the armor of " + lvl + " level");
     }
